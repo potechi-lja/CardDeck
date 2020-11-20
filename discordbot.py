2c64399a3f78452036328d5c42c849550a11ec7c
potechi <73507882+potechi-lja@users.noreply.github.com>
2020-11-21 00:21:09 +0900

    ステータス表示を追加、ダイス機能アップデート

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ import traceback
 import random
 import math
 import typing
+import re
 
 bot = commands.Bot(command_prefix='/')
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -22,11 +23,37 @@ async def on_command_error(ctx, error):
 # 起動時に動作する処理
 @bot.event
 async def on_ready():
+    # botのステータスを設定
+    activity = discord.Game(name='人生')
+    await bot.change_presence(activity=activity)
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    
+
+# ダイス処理関数
+def diceroll(dice):
+    # フォーマット整理：面数指定なしはD6とする
+    pattern_NDblank = "D(?![1-9])"
+    pattern_NDN = "[1-9][0-9]*D[1-9][0-9]*"
+    dice = dice.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
+    dice = dice.strip().replace('　', ' ').replace(' ', '').upper()
+    dice = re.sub(pattern_NDblank, 'D6', dice)
+    diceset = re.findall(pattern_NDN, dice)
+    dsumlist, dielist, result1, result2 = [], [], dice, dice
+
+    for n in diceset:
+        rolls, limit = map(int, n.split('D'))
+        die = [random.randint(1, limit) for r in range(rolls)]
+        dsum = sum(die)
+        dsumlist.append(dsum)
+        dielist.append(str(die).replace(' ',''))
+    for m in range(len(diceset)):
+        result1 = result1.replace(diceset[m], str(dsumlist[m]) + dielist[m], 1)
+        result2 = result2.replace(diceset[m], str(dsumlist[m]), 1)
+    total = eval(result2)
+    return [dice, result1, result2, str(total)]
+
 # ねこちゃんが返事をする
 @bot.command()
 async def neko(ctx):
@@ -37,20 +64,54 @@ async def neko(ctx):
     print('------')
 
 # ダイスを振る
-@bot.command()
 async def roll(ctx, dice : str):
-    """NdN形式でダイスを振ります。"""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    li = []
-    for r in range(rolls):
-        li.append(random.randint(1, limit))
-    result = ','.join(map(str, li))
-    await ctx.send(dice + ' -> [' + result + '] -> ' + str(sum(li)))
+    """入力されたダイスを振ります。`CoC`または`CoC6`と入力するとクトゥルフ6版、`CoC7`と入力するとクトゥルフ7版の探索者を自動生成します。"""
+    PATTERN_rollset = '\[.+?\]'
+    if dice == "CoC" or dice == "CoC6":
+        throw = '3d+3d+3d+3d+3d+2d+2d+3d'
+        try:
+            dice_return = diceroll(throw)
+        except Exception:
+            await ctx.send('入力が間違っているかもしれません……')
+            return
+        rollset = re.findall(PATTERN_rollset, dice_return[1])
+        status = ['STR', 'CON', 'POW', 'DEX', 'APP', 'SIZ', 'INT', 'EDU']
+        dice_sum = dice_return[2].split('+')
+        output_CoC = ''
+        for x in range(len(status)):
+            if status[x] in ['SIZ', 'INT']:
+                output_CoC += status[x] + ' -> ' + rollset[x] + '   +6 -> ' + str(int(dice_sum[x])+6) + '\n'
+            elif status[x] =='EDU':
+                output_CoC += status[x] + ' -> ' + rollset[x] + ' +3 -> ' + str(int(dice_sum[x])+3) + '\n'
+            else:
+                output_CoC += status[x] + ' -> ' + rollset[x] + '    -> ' + str(int(dice_sum[x])) + '\n'
+        output = 'クトゥルフ神話TRPG[6版]\n' + output_CoC + 'ダイス合計：' + str(dice_return[3])
+    elif dice == 'CoC7':
+        throw = '3d+3d+3d+3d+3d+2d+2d+2d+3d'
+        try:
+            dice_return = diceroll(throw)
+        except Exception:
+            await ctx.send('入力が間違っているかもしれません……')
+            return
+        rollset = re.findall(PATTERN_rollset, dice_return[1])
+        status = ['STR', 'CON', 'POW', 'DEX', 'APP', 'SIZ', 'INT', 'EDU', 'LUK']
+        dice_sum = dice_return[2].split('+')
+        output_CoC = ''
+        for x in range(len(status)):
+            if status[x] in ['SIZ', 'INT', 'EDU']:
+                output_CoC += status[x] + ' -> ' + rollset[x] + '   +6 ×5 -> ' + str(int(dice_sum[x])*5) + '\n'
+            else:
+                output_CoC += status[x] + ' -> ' + rollset[x] + '    ×5 -> ' + str(int(dice_sum[x])*5) + '\n'
+        output = 'クトゥルフ神話TRPG[7版]\n' + output_CoC + 'ダイス合計：' + str(dice_return[3])
+    else:
+        try:
+            dice_return = diceroll(dice)
+        except Exception:
+            await ctx.send('入力が間違っているかもしれません……')
+            return
+        output = dice_return[0] + '\n -> ' + dice_return[1] + '\n -> ' + dice_return[2] + '\n -> ' + dice_return[3]
+        print(output)
+    await ctx.send('```\n' + output + '```')
     print('command roll is send successfully.')
     print('------')
 
